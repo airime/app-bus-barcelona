9ddcd9c5711615fef248f41d9555ec88063b05ee
S-Cesc <cesc.sasal@gmail.com>
2024-07-06 16:24:33 +0200

    Merge de la branca "cesc". Alguns problemes pendents

---
+++ src/app/api.key.ts
@@ -0,0 +1,11 @@
+export const apiKey = "AIzaSyCcX1PIyoBrmJq5OeIwwwH8Sv1SICuKBno";
+export const FirebaseGeneratedApiKey = "AIzaSyAa4g9PQs_0cyvZxrd9PeibHvcRoKKgD18"; //Google maps restringida
+export const googleMapsApiKey = "AIzaSyA5GYdSFjVaEqkAhTZJRSRAKZJaK05PZ0k" //no restringida
+export const mapId = "8469ded0e454bb9b"
+export const tmbApiId = "03f33b26";
+export const tmbApiKey = "c17d88691aa120b4c5babbdd98be19a4";
+
+
+/* TODO   Cal establir quotes i alertes de GoogleMaps
+Google maps ja explica que es poden posar alertes, però no es pot restringir el consum
+*/

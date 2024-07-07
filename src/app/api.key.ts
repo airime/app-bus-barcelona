ba366323e9b8380ea9c6b38281864ac3ef9c9fe0
S-Cesc <cesc.sasal@gmail.com>
2024-07-07 08:18:46 +0200

    merge de la meva branca (branca cesc)

--- src/app/api.key.ts
+++ src/app/api.key.ts
@@ -1,11 +1,6 @@
-export const apiKey = "AIzaSyCcX1PIyoBrmJq5OeIwwwH8Sv1SICuKBno";
-export const FirebaseGeneratedApiKey = "AIzaSyAa4g9PQs_0cyvZxrd9PeibHvcRoKKgD18"; //Google maps restringida
-export const googleMapsApiKey = "AIzaSyA5GYdSFjVaEqkAhTZJRSRAKZJaK05PZ0k" //no restringida
-export const mapId = "8469ded0e454bb9b"
-export const tmbApiId = "03f33b26";
-export const tmbApiKey = "c17d88691aa120b4c5babbdd98be19a4";
-
-
-/* TODO   Cal establir quotes i alertes de GoogleMaps
-Google maps ja explica que es poden posar alertes, però no es pot restringir el consum
-*/
+export const apiKeyFirebase = "AIzaSyCcX1PIyoBrmJq5OeIwwwH8Sv1SICuKBno";
+export const googleMapsApiKey = "AIzaSyA5GYdSFjVaEqkAhTZJRSRAKZJaK05PZ0k"; //vinculada amb mapId i activada amb pagament
+export const _oldGoogleMapsApiKey = "AIzaSyAa4g9PQs_0cyvZxrd9PeibHvcRoKKgD18"; // (lostkey?)
+export const googleMapId = "8469ded0e454bb9b"
+export const tmb_api_id = "03f33b26";
+export const tmb_api_key = "c17d88691aa120b4c5babbdd98be19a4";

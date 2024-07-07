0844ccf31bb89360ad27f937c6be61099f24c19a
Airime <mireiamardom@gmail.com>
2024-07-07 19:56:26 +0200

    delete secrets

--- src/app/api.key.ts
+++
@@ -1,6 +0,0 @@
-export const apiKeyFirebase = "AIzaSyCcX1PIyoBrmJq5OeIwwwH8Sv1SICuKBno";
-export const googleMapsApiKey = "AIzaSyA5GYdSFjVaEqkAhTZJRSRAKZJaK05PZ0k"; //vinculada amb mapId i activada amb pagament
-export const _oldGoogleMapsApiKey = "AIzaSyAa4g9PQs_0cyvZxrd9PeibHvcRoKKgD18"; // (lostkey?)
-export const googleMapId = "8469ded0e454bb9b"
-export const tmb_api_id = "03f33b26";
-export const tmb_api_key = "c17d88691aa120b4c5babbdd98be19a4";
